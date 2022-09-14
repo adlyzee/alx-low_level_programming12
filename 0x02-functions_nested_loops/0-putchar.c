28c0c9a446b5f06f85cc9fb54458024f35756c00
adlyzee <107926798+adlyzee@users.noreply.github.com>
2022-09-14 18:55:37 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,20 @@
-a
+#include "main.h"
+
+/**
+ * main - main block
+ * description: prints _putchar followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char printer[] = "_putchar";
+	int i = 0;
+
+	while (printer[i] != '\0')
+	{
+		_putchar(printer[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
