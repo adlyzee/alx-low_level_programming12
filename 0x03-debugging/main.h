8bd9160771b12c573911b2207215b58272a25ac6
adlyzee <107926798+adlyzee@users.noreply.github.com>
2022-09-14 18:09:23 +0100

    main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,12 @@
-#ifdef MAIN_H
-#define MAIN_H
+s (8 sloc)  196 Bytes
 
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int, int, int);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
 
-#endif /* MAIN_H */
+#endif
